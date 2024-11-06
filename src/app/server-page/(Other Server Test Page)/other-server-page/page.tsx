9ea6360bb0c27cc85a5bb4eb522414843ff8bfaf
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-11-05 20:48:43 -0600

    route grouping

---
+++ src/app/server-page/(Other Server Test Page)/other-server-page/page.tsx
@@ -0,0 +1,18 @@
+export default function otherServerPage() {
+  const date = new Date();
+  const time = date.getTime();
+  const isInteger = Number.isInteger(time / 2);
+  const res = isInteger + "";
+
+  return (
+    <div>
+      <h1>
+        This is <i>another</i> server page.
+      </h1>
+      <p>
+        The time was divided by 2 and then to checked to see if it was an
+        integer. The result was: {res}.
+      </p>
+    </div>
+  );
+}
