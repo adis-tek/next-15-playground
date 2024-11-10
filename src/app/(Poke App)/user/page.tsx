74c75b471df922aac4c744d92b1e859a0a01da71
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-11-09 20:29:14 -0600

    supabase setup functional

---
+++ src/app/(Poke App)/user/page.tsx
@@ -0,0 +1,10 @@
+import UserInformation from "../ui/user-information";
+
+export default function userPage() {
+  return (
+    <div>
+      <h1>User Page</h1>
+      {/* <UserInformation props={} /> */}
+    </div>
+  );
+}
---
+++ src/app/ui/user-information.tsx
@@ -0,0 +1,7 @@
+export default function UserInformation(props: any) {
+  return (
+    <div>
+      <h1>User Info</h1>
+    </div>
+  );
+}
