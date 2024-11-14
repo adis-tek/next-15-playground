dc3f0457a04a1fe74013b3bda64f49a6da1ee839
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-11-13 22:46:48 -0600

    edge function successfully creates new stripe customer

--- supabase/functions/create-new-stripe-customer/index.ts
+++ supabase/functions/create-new-stripe-customer/index.ts
@@ -4,23 +4,41 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import Stripe from "https://esm.sh/stripe?target=deno";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.46.1";
 
-console.log("Hello from Functions!")
+const stripe = Stripe(Deno.env.get("STRIPE_KEY")!, {
+  httpClient: Stripe.createFetchHttpClient(),
+});
+
+const supabase = createClient(
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
+);
 
 Deno.serve(async (req) => {
   const { record } = await req.json();
 
-  const data = {
-    record,
-  }
+  const customer = await stripe.customers.create({
+    email: record.email,
+    metadata: {
+      supabase_id: record.id,
+    },
+  });
 
-  console.log({record});
+  const { data, error } = await supabase
+    .from("user_data")
+    .update({
+      stripe_customer_id: customer.id,
+    })
+    .match({ id: record.id });
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  console.log({ data, error, customer });
+
+  return new Response(JSON.stringify({ stripe_customer_id: customer.id }), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
 
 /* To invoke locally:
 
